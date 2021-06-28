21573ff36afbffbe1ae1167275c9801698145b85
EkaterinaLysova <84503629+EkaterinaLysova@users.noreply.github.com>
2021-06-29 02:24:08 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,5 +2,10 @@
 
 int main(int    argc, char  *argv[])
 {
-    
+    char map[x][y];
+
+    if (map[x == y])
+
+    else if (map != ) // если не равно квадрату или нельзя вывести квадрат, то :
+        write (2, "map error", 9); // вывести это в отдельную функцию
 }
