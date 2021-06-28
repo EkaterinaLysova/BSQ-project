5c8280bc63fac384121a64a842061ef573926f57
EkaterinaLysova <84503629+EkaterinaLysova@users.noreply.github.com>
2021-06-28 18:46:52 +0300

    Create basic_functions.c

---
+++ basic_functions.c
@@ -0,0 +1,87 @@
+#include <unistd.h>
+#include "ft_bsq.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nbr)
+{
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	ft_putchar((nbr % 10) + '0');
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_atoi(char *s)
+{
+	int		sign;
+	long	r;
+
+	r = 0;
+	sign = 1;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		r = r * 10 + *s - '0';
+		s++;
+	}
+	return (sign * (int)r);
+}
+
+void	ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest[i]);
+}
+
+/*void	ft_put_result(char *str, int start)
+{
+	int		i;
+
+	i = start;
+	while (src[i])
+		i++;
+	write(1, &str[start], i - start);
+}
+/*
