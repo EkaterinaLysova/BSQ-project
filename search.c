d735162bdf3b8f42bbe031178a06b6df1778b1a3
EkaterinaLysova <84503629+EkaterinaLysova@users.noreply.github.com>
2021-06-29 02:23:47 +0300

    Update search.c

--- search.c
+++ search.c
@@ -2,7 +2,7 @@
 
 int ft_search_place (int a, char *mass[])
 {
-    char a;
+    char a; // здесь мы указываем препятствия
     int count; // а здесь - положение конкретного печатаемого символа
     int x; // это мы считаем длину массива по горизонтали
     int y; // а это - длина по вертикали
@@ -17,7 +17,36 @@ int ft_search_place (int a, char *mass[])
         count++;
     
 
-    else if(mass[y][count] == "." && mass[y + 1][count] == ".")
+    else if(mass[y][count] == "." && (mass[y + 1][count] != "o" || mass[y + 1][count] != "\n"))
         write (1, "x", 1);
 
+/*Начните с верхнего левого угла. Посмотрите, сработает ли квадрат 1x1.
+
+Если это сработает, увеличьте длину сторон квадрата на 1 и повторите.
+Если это не сработает, двигайтесь вправо и повторите. Если вы достигли 
+самой правой позиции, перейдите к следующей строке.
+}*/
+
+int	ft_recursive_for_square(int nb, int power) // функция возвращает значение степени, примененной к числу. Изменено под нужные нам параметры.
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb + (ft_recursive_for_square(nb, power - 1)));
+}
+
+int max_square (int lenght, int *value)
+{
+    char square; 
+    int size;
+
+    square = [matrix * lenght];
+    size = 0;
+
+    if (size == 0)
+        if (size =="o")
+            size++;
+        else
+            size = size + 1;
 }
